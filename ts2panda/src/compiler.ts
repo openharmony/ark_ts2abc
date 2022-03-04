842e715b7901472911346d0a8cf89125b6c7cbde
changjiaxing <changjiaxing1@huawei.com>
2022-03-04 10:13:17 +0800

    Signed-off-by: changjiaxing <changjiaxing1@huawei.com>
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is up to date with 'origin/master'.

--- ts2panda/src/compiler.ts
+++ ts2panda/src/compiler.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -38,6 +38,7 @@ import { CompilerDriver } from "./compilerDriver";
 import { DebugInfo, NodeKind } from "./debuginfo";
 import { DiagnosticCode, DiagnosticError } from "./diagnostic";
 import { compileArrayLiteralExpression } from "./expression/arrayLiteralExpression";
+import { compileBigIntLiteral } from "./expression/bigIntLiteral";
 import {
     compileCallExpression,
     getHiddenParameters
@@ -782,6 +783,7 @@ export class Compiler {
                 compileNumericLiteral(this.pandaGen, <ts.NumericLiteral>expr);
                 break;
             case ts.SyntaxKind.BigIntLiteral: // line 35
+                compileBigIntLiteral(this.pandaGen, <ts.BigIntLiteral>expr);
                 break;
             case ts.SyntaxKind.StringLiteral: // line 36
                 compileStringLiteral(this.pandaGen, <ts.StringLiteral>expr);
